6b827f7fc0d0eae86d36c40faee97776ae4b8ce8
Viperr <90527311+froncoi@users.noreply.github.com>
2023-07-20 14:11:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function redirectToLogin() {
-  window.location.href = "/login";
+  // Replace 'YOUR_SERVER_URL' with the URL of your Flask server
+  const serverUrl = 'https://discord.gg/E4vnewmZ33';
+  const clientId = '1090971580357673084';
+  const redirectUri = encodeURIComponent(`${serverUrl}/callback`);
+
+  // Redirect the user to the Discord OAuth2 authorization URL
+  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=identify%20guilds%20email`;
 }
+
 
 const bgAnimation = document.getElementById("bgAnimation");
 const numberOfColorBoxes = 400;
