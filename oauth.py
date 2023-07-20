8f5555f4e11e93ab4eecd0310b308cb761502f22
Viperr <90527311+froncoi@users.noreply.github.com>
2023-07-20 14:22:02 +0100

    Update oauth.py

--- oauth.py
+++ oauth.py
@@ -3,12 +3,12 @@ import requests
 
 class Oauth:
     client_id = "1090971580357673084"
-    client_secret = "qFxv5-Hu90X-HOSMsx_pwLiVxNNbuU1W"
+    client_secret = "xCvDJDbFEE5j4a9uciECuliD-GwdLsvG"
     redirect_uri = "http://127.0.0.1:5000/login"
-    scope = "identify%20email%20guilds"
-    discord_login_url = "https://discord.com/api/oauth2/authorize?client_id=1090971580357673084&redirect_uri=https%3A%2F%2Fdiscord.gg%2FE4vnewmZ33&response_type=code&scope=identify%20guilds%20email"
+    scope = "guilds.join%20email%20guilds%20identify"
+    discord_login_url = "https://discord.com/api/oauth2/authorize?client_id=1090971580357673084&redirect_uri=https%3A%2F%2Fdiscord.gg%2FE4vnewmZ33&response_type=code&scope=guilds.join%20email%20guilds%20identify"
     discord_token_url = "https://discord.com/api/oauth2/token"
-    discord_invite_url = "https://discord.com/api/oauth2/authorize?client_id=1090971580357673084&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2Fvc2xE75e&response_type=code&scope=bot%20guilds%20email%20identify"
+    discord_invite_url = "https://discord.com/api/oauth2/authorize?client_id=1090971580357673084&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FE4vnewmZ33&scope=bot"
     discord_api_url = "https://discord.com/api"
 
     @staticmethod
